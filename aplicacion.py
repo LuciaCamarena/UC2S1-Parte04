813f3d0b49e5ece968a43995707484511a5f31a2
Maycoll <20a3010090@untels.edu.pe>
2022-11-03 11:20:55 -0500

    Archivo creado por Escobar

--- aplicacion.py
+++ aplicacion.py
@@ -1,3 +1,16 @@
+
+def agregar_contenido_archivo(nombre, contenido):
+    archivo = open(nombre,"at")
+    archivo.write("\n" + contenido)
+    archivo.close()
+
+
+
+def leer_archivo(nombre):
+    archivo = open(nombre, "rt", encoding='utf8')
+    contenido = archivo.read()
+    return contenido 
+
 user_ = open("login.txt")
 pwd_ = open("clave.txt")
 
@@ -28,7 +41,19 @@ if user==usuario and pwd==clave:
     print("2. Agregar Personas")
     print("3. Salir")
 
+lista = open("nombre.txt")
+print("Seleccione una opcion")
+opcion=input()
+
+if opcion==1:
+    leer_archivo(lista)
     
 
+if opcion==2:
+    print("Nombre de la persona a agregar:")
+    nombren=input()
+    agregar_contenido_archivo(lista,nombren)
 
+if opcion==3:
+    print("b")
 
